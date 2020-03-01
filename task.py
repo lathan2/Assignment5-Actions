2a8343ca3b1aab9fc6bf7b9a6a7c738675613246
lathanliles <lathanliles@gmail.com>
2020-02-29 17:07:03 -0800

    added circle radius function and tests for that function

--- task.py
+++ task.py
@@ -1,2 +1,12 @@
+import math
+
 def firstrun():
     return "success"
+
+
+def circle_area(radius):
+    if (radius < 0):
+        return -1
+    
+    else:
+        return math.pi * math.pow(radius, 2)
