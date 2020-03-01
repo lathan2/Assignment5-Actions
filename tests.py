beedea98ce9745a5ae0238751b1cd11ce7113663
lathanliles <lathanliles@gmail.com>
2020-02-29 17:14:02 -0800

    fixed linterissues

--- tests.py
+++ tests.py
@@ -8,9 +8,11 @@ class TestCase(unittest.TestCase):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
 
+
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
 
     def test_circle(self):
         expected_0 = math.pi
@@ -19,14 +21,14 @@ class TestCase(unittest.TestCase):
         expected_1 = math.pi * math.pow(14, 2)
         self.assertEqual(expected_1, task.circle_area(14))
 
-        expected_2 = 0 
-        self.assertEqual(expected_2, task.circle_area(0)) 
+        expected_2 = 0
+        self.assertEqual(expected_2, task.circle_area(0))
 
-        expected_3 = math.pi * math.pow(123456789, 2)  
-        self.assertEqual(expected_3, task.circle_area(123456789)) 
+        expected_3 = math.pi * math.pow(123456789, 2)
+        self.assertEqual(expected_3, task.circle_area(123456789))
 
-        expected_4 = -1  
-        self.assertEqual(expected_4, task.circle_area(-5)) 
+        expected_4 = -1
+        self.assertEqual(expected_4, task.circle_area(-5))
 
 
 if __name__ == '__main__':
