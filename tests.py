6828d6fbe09b751f94bed9fafdb5786357964413
lathanliles <lathanliles@gmail.com>
2020-02-29 18:14:12 -0800

    function 3 - difference dates

--- tests.py
+++ tests.py
@@ -64,6 +64,9 @@ class TestCase(unittest.TestCase):
         expected_2 = 9422
         self.assertEqual(expected_2, task.difference_dates(date(1994, 5, 14), date(2020, 2, 29)))
 
+        expected_3 = -737483
+        self.assertEquals(expected_3, task.difference_dates(date(2020, 2, 29), date(1,1,1)))
+
 
 if __name__ == '__main__':
     unittest.main()
